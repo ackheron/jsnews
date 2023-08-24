d248f510132dcdf2133090efe6f3e4f931e087b4
Ackheron <ackheron@etik.com>
2023-08-24 16:32:24 +0200

    fix affichage des dates

--- frontend/js/article.js
+++ frontend/js/article.js
@@ -3,17 +3,17 @@
  */
 
 class Article {
-    // Constructeur qui accepte un objet jsonArticle en argument
-    constructor(jsonArticle) {
-        // Si jsonArticle est fourni et est truthy, fusionne ses propriétés avec l'instance courante
+    // Constructeur qui accepte un objet article en argument
+    constructor(article) {
+        // Si article est fourni et est truthy, fusionne ses propriétés avec l'instance courante
         /*
 
-        jsonArticle && ...: Ceci est une expression logique qui vérifie d'abord si jsonArticle est truthy (c'est-à-dire, il n'est pas falsy comme null, undefined, false, 0, NaN, "", etc.). Si jsonArticle est falsy, alors la deuxième partie après && ne sera pas exécutée.
-        Object.assign(this, jsonArticle): Cette méthode est utilisée pour copier les valeurs de toutes les propriétés énumérables de l'objet jsonArticle vers l'objet this (qui est l'instance courante de la classe article). Si une propriété du même nom existe déjà sur l'objet this, sa valeur sera écrasée par la valeur correspondante de jsonArticle.
+        article && ...: Ceci est une expression logique qui vérifie d'abord si article est truthy (c'est-à-dire, il n'est pas falsy comme null, undefined, false, 0, NaN, "", etc.). Si article est falsy, alors la deuxième partie après && ne sera pas exécutée.
+        Object.assign(this, article): Cette méthode est utilisée pour copier les valeurs de toutes les propriétés énumérables de l'objet article vers l'objet this (qui est l'instance courante de la classe article). Si une propriété du même nom existe déjà sur l'objet this, sa valeur sera écrasée par la valeur correspondante de article.
           */
-        jsonArticle && Object.assign(this, jsonArticle);
+        article && Object.assign(this, article);
     }
-    getFormattedDate(article) {
+    getFormattedDate() {
         // Crée un objet Date à partir de la date de publication de l'objet courant
         let date = new Date(this.publicationDate);
         // Renvoie la date formatée selon le format français avec des options spécifiques
--- frontend/view/home/home.js
+++ frontend/view/home/home.js
@@ -1,34 +1,40 @@
 /**
- * Gère l'affichage et les interactions de la page d'accueil
+ * Récupère les articles depuis une API, crée des instances d'articles,
+ * puis les affiche sur la page.
  */
-
 const fetchData = async () => {
     try {
+        // Envoie une requête GET pour récupérer les données des articles
         const response = await axios.get("http://localhost:4000/api/article/");
 
         console.log(response.data);
+        // Stocke les données renvoyées par l'API
         const data = response.data;
 
-        for (const jsonArticle of data) {
-            let article = new Article(jsonArticle);
+        // Parcourt chaque article et l'affiche sur la page
+        for (const arrayArticle of data) {
+            // Crée une instance de l'objet Article à partir des données du tableau d'objets envoyer par l'API
+            let article = new Article(arrayArticle);
+
+            // Génère le HTML pour chaque article et l'ajoute au conteneur
             document.querySelector(".container").innerHTML += `
-
-    <div class="col-12 mt-5">
-        <div class="card article">
-            <div class="card-header ">
-                <h5 class="card-title d-flex justify-content-between">${article.title}<span class="publication-date">${article.publicationDate}</span></h5>
-            </div>
-            <img src="http://localhost:4000/${article.image}" class="card-img-top">
-            <span class="fa-stack fa-2x addFavorite">
-                <i class="fas fa-star fa-stack-1x"></i>
-                <i class="far fa-star fa-stack-1x"></i>
-            </span>
-            <div class="card-body">
-                <p class="card-text">${article.content}</p>
-            </div>
-        </div>
-    </div>
-
+                <div class="col-12 mt-5">
+                    <div class="card article">
+                        <div class="card-header ">
+                            <h5 class="card-title d-flex justify-content-between">${article.title}<span class="publication-date">${article.getFormattedDate(
+                article.publicationDate
+            )}</span></h5>
+                        </div>
+                        <img src="http://localhost:4000/${article.image}" class="card-img-top">
+                        <span class="fa-stack fa-2x addFavorite">
+                            <i class="fas fa-star fa-stack-1x"></i>
+                            <i class="far fa-star fa-stack-1x"></i>
+                        </span>
+                        <div class="card-body">
+                            <p class="card-text">${article.content}</p>
+                        </div>
+                    </div>
+                </div>
             `;
         }
     } catch (error) {
